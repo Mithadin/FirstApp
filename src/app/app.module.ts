c0379363c8cbb7fe121a9ee91d1134ecc56068c0
Mithadin <mithadin.c@outlook.com>
2020-10-31 19:59:25 +0530

    Added form feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  
- }
+export class AppModule {}
